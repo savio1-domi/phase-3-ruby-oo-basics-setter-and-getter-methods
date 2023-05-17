2443e1150dc3f3a7a7a173c2faae7bb983cd1f82
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-17 10:28:03 +0300

    Completed assignments

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
-
+    # setters
+    def name=(name)
+        @name = name
+    end   
+     
+    
+    def bread=(bread)
+        @bread = bread
+    end 
+    def breed=(dog_breed)
+        @breed = dog_breed     
+    end
+    def breed
+        @breed
+    end
+    snoopy = Dog.new
+    snoopy.breed = "Beagle"
+    puts snoopy.breed
+    
+    # Getters of you can use: attr_reader :name, :breed
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    # attr_reader :name, :job
 
+    # Setters
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+
+    # Getters or you can use: attr_reader :name, :job
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 end
+kanye = Person.new
